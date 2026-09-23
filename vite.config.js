1286578b14797e63393d282d776113230b21b286
Ayoade Victoria <ayoadev750@gmail.com>
2026-09-23 03:11:46 +0100

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/storyworld/',
+  base: '/storyworld/', 
+  build: {
+    outDir: 'docs',
+  },
   server: {
     hmr: {
       overlay: false,
